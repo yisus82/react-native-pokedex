96335cb106c0381a6a2f25748d05df20fe55e2f7
yisus82 <djalma8@gmail.com>
2019-07-24 14:02:31 +0200

    Updates

---
+++ precache-manifest.a491e8d300a7c6169b5500bd97c83676.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c19fefbe76e5504d38f71736554ec7d",
+    "url": "/yisus82/react-native-pokedex/index.html"
+  },
+  {
+    "revision": "2e172f44b4a80a732782",
+    "url": "/yisus82/react-native-pokedex/static/css/main.0d11f04c.chunk.css"
+  },
+  {
+    "revision": "535f7cf80e9bdcaa5a25",
+    "url": "/yisus82/react-native-pokedex/static/js/2.078b2701.chunk.js"
+  },
+  {
+    "revision": "2e172f44b4a80a732782",
+    "url": "/yisus82/react-native-pokedex/static/js/main.9267da9c.chunk.js"
+  },
+  {
+    "revision": "29130caad2e589733ca1",
+    "url": "/yisus82/react-native-pokedex/static/js/runtime~main.e2bd2cf6.js"
+  }
+]);
